e9300cb3e8ab346b89e904cbab2a04040c010cac
Ayush Raj <404ayushraj@gmail.com>
2026-09-03 20:37:57 +0530

    Time: 4 ms (7.92%) | Memory: 19.4 MB (62.22%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -7,21 +7,26 @@ class Solution:
             return 0
         if m==n==1:
             return 1
+
         dp=[[0] * n for _ in range(m)]
+
         for i in range(n):
             if obs[0][i]==1:
                 break
-            dp[0][i]=1
+            else:
+                dp[0][i]=1
         for j in range(m):
             if obs[j][0]==1:
                 break
-            dp[j][0]=1
+            else:
+                dp[j][0]=1
+
         dp[0][0]=0
 
         for i in range(1,m):
             for j in range(1,n):
-                if obs[i][j]==0:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
-                else:
+                if obs[i][j]==1:
                     dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1]
